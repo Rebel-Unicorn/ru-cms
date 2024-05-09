b5dc6ac34293892bc92faceae8f642d237fdbb55
Chukwu <chukwudaluay@gmail.com>
2024-05-09 08:18:27 +0100

    feat: 🎸 connect to neondb and cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,25 @@
-module.exports = () => ({});
+module.exports = ({env}) => ({
+  'strapi-neon-tech-db-branches': {
+    enabled: true,
+    config: {
+      neonApiKey: env('NEON_APIKEY'), // get it from here: https://console.neon.tech/app/settings/api-keys
+      neonProjectName: env('NEON_PROJECT_NAME'), // the neon project under wich your DB runs
+      neonRole: env('NEON_ROLE'), // create it manually under roles for your project first
+      //(gitBranch: "main") // branch can be pinned via this config option. Will not use branch from git then. Usefull for preview/production deployment
+    }
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
