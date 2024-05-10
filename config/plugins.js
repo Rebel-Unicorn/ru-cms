be9f274bfc4c1846962f940612cd5bd05c5bf3fc
Chukwu <chukwudaluay@gmail.com>
2024-05-10 07:41:30 +0100

    ci: 🎡 fix deployment

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,4 @@
 module.exports = ({env}) => ({
-  'strapi-neon-tech-db-branches': {
-    enabled: true,
-    config: {
-      neonApiKey: env('NEON_APIKEY'), // get it from here: https://console.neon.tech/app/settings/api-keys
-      neonProjectName: env('NEON_PROJECT_NAME'), // the neon project under wich your DB runs
-      neonRole: env('NEON_ROLE'), // create it manually under roles for your project first
-      //(gitBranch: "main") // branch can be pinned via this config option. Will not use branch from git then. Usefull for preview/production deployment
-    }
-  },
   upload: {
     config: {
       provider: 'cloudinary',
